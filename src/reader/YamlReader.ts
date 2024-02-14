d57c0011e08eb50c23447f47aa743e017fd06cb3
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2024-02-14 07:25:48 +0100

    initial

--- src/reader/YamlReader.ts
+++ src/reader/YamlReader.ts
@@ -1,43 +1,43 @@
 import { type Application, type IReader, type Module, type Schema, type SchemaTyp } from './IReader'
 import { type Config } from '../config/Config'
 
-export class DefaultReader implements IReader {
+export class YamlReader implements IReader {
   readonly #application?: Application | undefined = undefined
   readonly #modules: Module[] | undefined = undefined
   readonly #schemas?: Schema[] | undefined = undefined
 
   constructor (private readonly config: Config) {}
 
-  async read (): Promise<void> {
+  public async read (): Promise<void> {
     // TODO: Implement
   }
 
-  getApplication (): Application {
+  public getApplication (): Application {
     if (this.#application === undefined) {
       throw new Error('Reader not finished')
     }
     return this.#application
   }
 
-  getModules (): Module[] {
+  public getModules (): Module[] {
     if (this.#modules === undefined) {
       throw new Error('Reader not finished')
     }
     return this.#modules
   }
 
-  getSchemas (): Schema[] {
+  public getSchemas (): Schema[] {
     if (this.#schemas === undefined) {
       throw new Error('Reader not finished')
     }
     return this.#schemas
   }
 
-  getSchemasForModule (module: Module): Schema[] {
+  public getSchemasForModule (module: Module): Schema[] {
     return this.getSchemas().filter(s => s.$id.startsWith(module.$id))
   }
 
-  getSchemasForModuleAndTyp (module: Module, typ: SchemaTyp): Schema[] {
+  public getSchemasForModuleAndTyp (module: Module, typ: SchemaTyp): Schema[] {
     return this.getSchemasForModule(module).filter(s => s['x-schema-typ'] === typ)
   }
 }
