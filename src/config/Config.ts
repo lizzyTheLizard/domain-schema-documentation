e0a01369ec9af6d3312278b7374af25cff66c224
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2024-02-14 07:29:10 +0100

    initial

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,4 +1,6 @@
 import { type IReader } from '../reader/IReader'
+import { type IWriter } from '../writer/IWriter'
+import { type IVerifier } from '../verifier/IVerifier'
 
 export interface Config {
   inputFolder: string
