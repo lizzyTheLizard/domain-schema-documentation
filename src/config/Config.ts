30f64cc3512115af175aa132a22b7c8ab021a9ee
grma <matthias.graf@zuehlke.com>
2024-02-14 09:57:20 +0100

    initial

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,10 +1,12 @@
 import { type IReader } from '../reader/IReader'
 import { type IWriter } from '../writer/IWriter'
 import { type IVerifier } from '../verifier/IVerifier'
+import {Options} from "ajv";
 
 export interface Config {
   inputFolder: string
   outputFolder: string
+  ajvOps?: Options
   reader: IReader
   writer: IWriter[]
   verifier: IVerifier[]
--- src/reader/YamlReader.ts
+++ src/reader/YamlReader.ts
@@ -1,16 +1,87 @@
 import { type Application, type IReader, type Module, type Schema, type SchemaTyp } from './IReader'
 import { type Config } from '../config/Config'
+import { promises as fs } from 'fs'
+import path from 'path'
+import * as yaml from 'yaml'
+import Ajv from "ajv";
+import moduleSchema from './moduleSchema.json';
+import applicationSchema from './applicationSchema.json';
 
 export class YamlReader implements IReader {
-  readonly #application?: Application | undefined = undefined
-  readonly #modules: Module[] | undefined = undefined
-  readonly #schemas?: Schema[] | undefined = undefined
+  #application?: Application | undefined = undefined
+  #modules: Module[] | undefined = undefined
+  #schemas?: Schema[] | undefined = undefined
+  readonly #ajv: Ajv;
 
-  constructor (private readonly config: Config) {}
+  constructor (private readonly config: Config) {
+    this.#ajv = new Ajv(config.ajvOps ?? {allErrors: true});
+  }
 
   public async read (): Promise<void> {
-    // TODO: Implement
+    const applicationFile = path.join(this.config.inputFolder, 'index.yaml');
+    this.#application = await this.loadApplicationFile(applicationFile);
+
+    this.#modules = [];
+    this.#schemas = [];
+    await this.readFolderRecursive(this.config.inputFolder);
+    this.validate();
   }
+
+  private async readFolderRecursive(folder: string): Promise<void> {
+    const files = await fs.readdir(folder);
+    for (const file of files) {
+      const filePath = path.join(folder, file);
+      const lstat = await fs.lstat(filePath);
+      if (!lstat.isDirectory()) {
+        await this.readFolderRecursive(filePath);
+      } else if (file === "index.yaml") {
+        const module = await this.readModuleFile(filePath);
+        this.#modules?.push(module);
+      } else if (file.endsWith(".yaml")) {
+        const schema = await this.readSchemaFile(filePath);
+        this.#schemas?.push(schema);
+      } else {
+        throw new Error(`Unexpected file ${filePath}. All files must have .yaml ending`)
+      }
+    }
+  }
+
+  protected async loadApplicationFile(filePath: string): Promise<Application> {
+    const contend = await fs.readFile(filePath);
+    const parsed = yaml.parse(contend.toString());
+    this.validateApplicationSchema(parsed, filePath);
+    return parsed;
+  }
+
+  protected validateApplicationSchema(parsed: any, filePath: string): void {
+    if(!this.#ajv.validate(applicationSchema, parsed)) {
+      console.log(this.#ajv.errors);
+      throw new Error(`Invalid application file ${filePath}: ${this.#ajv.errorsText(this.#ajv.errors)}`);
+    }
+  }
+
+  protected async readModuleFile(filePath: string): Promise<Module> {
+    const contend = await fs.readFile(filePath);
+    const parsed = yaml.parse(contend.toString());
+    this.validateModuleSchema(parsed, filePath);
+    return parsed;
+  }
+
+  protected validateModuleSchema(parsed: any, filePath: string): void {
+    if(!this.#ajv.validate(moduleSchema, parsed)) {
+      console.log(this.#ajv.errors);
+      throw new Error(`Invalid module file ${filePath}: ${this.#ajv.errorsText(this.#ajv.errors)}`);
+    }
+  }
+
+  protected async readSchemaFile(filePath: string): Promise<Schema> {
+    const contend = await fs.readFile(filePath);
+    const parsed = yaml.parse(contend.toString());
+    this.validateSchemaSchema(parsed, filePath);
+    return parsed;
+  }
+
+
 
   public getApplication (): Application {
     if (this.#application === undefined) {
